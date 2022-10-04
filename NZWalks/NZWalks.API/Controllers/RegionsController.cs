6695cd9099baa735a88aecd522caf2e84f47c378
Viktor Toman <vtoman@gmail.com>
2022-10-04 14:32:56 +0200

    Using automapper for DTO

--- NZWalks/NZWalks.API/Controllers/RegionsController.cs
+++ NZWalks/NZWalks.API/Controllers/RegionsController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
 using NZWalks.API.Models.Domain;
 using NZWalks.API.Repositories;
 
@@ -9,10 +10,12 @@ namespace NZWalks.API.Controllers
     public class RegionsController : Controller
     {
         private readonly IRegionRepository regionRepository;
+        private readonly IMapper mapper;
 
-        public RegionsController(IRegionRepository regionRepository)
+        public RegionsController(IRegionRepository regionRepository, IMapper mapper)
         {
             this.regionRepository = regionRepository;
+            this.mapper = mapper;
         }
 
         [HttpGet]
@@ -20,22 +23,7 @@ namespace NZWalks.API.Controllers
         {
             var regions = regionRepository.GetAll();
 
-            var regionsDTO = new List<Models.DTO.Region>();
-            regions.ToList().ForEach(region =>
-            {
-                var regionDTO = new Models.DTO.Region()
-                {
-                    Id = region.Id,
-                    Code = region.Code,
-                    Name = region.Name,
-                    Area = region.Area,
-                    Lat = region.Lat,
-                    Long = region.Long,
-                    Population = region.Population
-                };
-
-                regionsDTO.Add(regionDTO);
-            });
+            var regionsDTO = mapper.Map<List<Models.DTO.Region>>(regions);
 
             return Ok(regionsDTO);
         }
