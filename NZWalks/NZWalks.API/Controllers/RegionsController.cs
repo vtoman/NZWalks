cff741db1eb98fc235817d9f16308c84b9af4c8f
Viktor Toman <vtoman@gmail.com>
2022-10-04 13:56:53 +0200

    Create action to return all regions (hardcoded)

---
+++ NZWalks/NZWalks.API/Controllers/RegionsController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RegionsController : Controller
+    {
+        [HttpGet]
+        public IActionResult GetAllRegions()
+        {
+            var regions = new List<Region>()
+            {
+                new Region
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Wellington",
+                    Code = "WLG",
+                    Area = 227755,
+                    Lat = -1.8822,
+                    Long = 299.88,
+                    Population = 500000
+                },
+                new Region
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Auckland",
+                    Code = "AUCK",
+                    Area = 227755,
+                    Lat = -1.8822,
+                    Long = 299.88,
+                    Population = 500000
+                 }
+            };
+
+            return Ok(regions);
+        }
+    }
+}
