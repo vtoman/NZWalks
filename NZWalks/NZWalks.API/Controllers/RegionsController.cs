fdbf5ef9f8234858a5a652b68b8fbfa7483ac233
Viktor Toman <vtoman@gmail.com>
2022-10-04 14:22:39 +0200

    Implementing repository pattern for regions

--- NZWalks/NZWalks.API/Controllers/RegionsController.cs
+++ NZWalks/NZWalks.API/Controllers/RegionsController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using NZWalks.API.Models.Domain;
+using NZWalks.API.Repositories;
 
 namespace NZWalks.API.Controllers
 {
@@ -7,34 +8,36 @@ namespace NZWalks.API.Controllers
     [Route("[controller]")]
     public class RegionsController : Controller
     {
+        private readonly IRegionRepository regionRepository;
+
+        public RegionsController(IRegionRepository regionRepository)
+        {
+            this.regionRepository = regionRepository;
+        }
+
         [HttpGet]
         public IActionResult GetAllRegions()
         {
-            var regions = new List<Region>()
-            {
-                new Region
-                {
-                    Id = Guid.NewGuid(),
-                    Name = "Wellington",
-                    Code = "WLG",
-                    Area = 227755,
-                    Lat = -1.8822,
-                    Long = 299.88,
-                    Population = 500000
-                },
-                new Region
-                {
-                    Id = Guid.NewGuid(),
-                    Name = "Auckland",
-                    Code = "AUCK",
-                    Area = 227755,
-                    Lat = -1.8822,
-                    Long = 299.88,
-                    Population = 500000
-                 }
-            };
+            var regions = regionRepository.GetAll();
 
-            return Ok(regions);
+            var regionsDTO = new List<Models.DTO.Region>();
+            regions.ToList().ForEach(region =>
+            {
+                var regionDTO = new Models.DTO.Region()
+                {
+                    Id = region.Id,
+                    Code = region.Code,
+                    Name = region.Name,
+                    Area = region.Area,
+                    Lat = region.Lat,
+                    Long = region.Long,
+                    Population = region.Population
+                };
+
+                regionsDTO.Add(regionDTO);
+            });
+
+            return Ok(regionsDTO);
         }
     }
 }
