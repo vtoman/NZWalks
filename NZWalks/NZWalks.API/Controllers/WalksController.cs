670e7e4b042a75c677976eb4d8a7e6e91018361d
Viktor Toman <vtoman@gmail.com>
2022-10-05 14:33:41 +0200

    Walk CRUD operations

---
+++ NZWalks/NZWalks.API/Controllers/WalksController.cs
@@ -0,0 +1,70 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Models.DTO;
+using NZWalks.API.Repositories;
+
+namespace NZWalks.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WalksController : Controller
+    {
+        private readonly IWalkRepository walkRepository;
+        private readonly IMapper mapper;
+
+        public WalksController(IWalkRepository walkRepository, IMapper mapper)
+        {
+            this.walkRepository = walkRepository;
+            this.mapper = mapper;
+        }
+
+        [HttpGet]        
+        public async Task<IActionResult> GetAllWalksAsync()
+        {
+            var walks = await walkRepository.GetAllAsync();
+            var walksDTO = mapper.Map<List<Models.DTO.Walk>>(walks);
+            return Ok(walksDTO);
+        }
+
+        [HttpGet]
+        [Route("{id:guid}")]
+        [ActionName("GetWalkAsync")]
+        public async Task<IActionResult> GetWalkAsync([FromRoute] Guid id)
+        {
+            var walk = await walkRepository.GetAsync(id);
+            if (walk == null) return NotFound();
+            var walkDTO = mapper.Map<Models.DTO.Walk>(walk);
+            return Ok(walkDTO);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddWalkAsync(Models.DTO.AddWalkRequest addWalkRequest)
+        {
+            var walk = mapper.Map<Models.Domain.Walk>(addWalkRequest);
+            walk = await walkRepository.AddAsync(walk);
+            var walkDTO = mapper.Map<Models.DTO.Walk>(walk);
+            return CreatedAtAction(nameof(GetWalkAsync), new { id = walkDTO.Id }, walkDTO);
+        }
+
+        [HttpDelete]
+        [Route("{id:guid}")]
+        public async Task<IActionResult> DeleteWalkAsync(Guid id)
+        {
+            var walk = await walkRepository.DeleteAsync(id);
+            if (walk == null) return NotFound();
+            var walkDTO = mapper.Map<Models.DTO.Walk>(walk);
+            return Ok(walkDTO);
+        }
+
+        [HttpPut]
+        [Route("{id:guid}")]
+        public async Task<IActionResult> UpdateWalk([FromRoute] Guid id, [FromBody] UpdateWalkRequest updateWalkRequest)
+        {
+            var walk = mapper.Map<Models.Domain.Walk>(updateWalkRequest);
+            walk = await walkRepository.UpdateAsync(id, walk);
+            if (walk == null) return NotFound();
+            var walkDTO = mapper.Map<Models.DTO.Walk>(walk);
+            return Ok(walkDTO);
+        }
+    }
+}
---
+++ NZWalks/NZWalks.API/Profiles/WalksProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+
+namespace NZWalks.API.Profiles
+{
+    public class WalksProfile : Profile
+    {
+        public WalksProfile()
+        {
+            CreateMap<Models.DTO.Walk, Models.Domain.Walk>().ReverseMap();  
+            CreateMap<Models.Domain.WalkDifficulty, Models.DTO.WalkDifficulty>().ReverseMap();
+
+            CreateMap<Models.DTO.AddWalkRequest, Models.Domain.Walk>();
+            CreateMap<Models.DTO.UpdateWalkRequest, Models.Domain.Walk>();
+        }
+    }
+}
